edcd7fb1de0fd6e3036caab82ce22b7f0984cf24
SND\cuipengfei_cp <SND\cuipengfei_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2012-08-08 14:46:57 +0000

    impl roster
    git-svn-id: https://mpjsp.svn.codeplex.com/svn@68536 142e9c35-602f-4880-9f37-ec48a24daa6a

--- emailserver/src/main/java/com/agiledon/agile/refactoring/email/FileRoster.java
+++ emailserver/src/main/java/com/agiledon/agile/refactoring/email/FileRoster.java
@@ -1,6 +1,8 @@
 package com.agiledon.agile.refactoring.email;
 
 import javax.mail.Address;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,14 +12,34 @@ import javax.mail.Address;
  * To change this template use File | Settings | File Templates.
  */
 public class FileRoster implements Roster {
+    private Address[] addresses;
+
     public FileRoster(String fileName) {
+        try {
+            addresses = new Address[]{
+                    new InternetAddress("abc@qq.com"),
+                    new InternetAddress("xyz@qq.com"),
+            };
+        } catch (AddressException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean constainsOneOf(Address[] from) {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
+        for (int i = 0; i < addresses.length; i++) {
+            Address address = addresses[i];
+            for (int j = 0; j < from.length; j++) {
+                Address fromAddress = from[j];
+                if (address.equals(fromAddress)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     public Address[] getAddresses() {
-        return new Address[0];  //To change body of implemented methods use File | Settings | File Templates.
+        return addresses;
     }
 }
