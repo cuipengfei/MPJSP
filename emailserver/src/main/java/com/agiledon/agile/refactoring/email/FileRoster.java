a2d0a4559eccbeb8b35f379f92ad0ad3d07d3f72
SND\cuipengfei_cp <SND\cuipengfei_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2012-08-08 13:36:40 +0000

    reorganize folders
    git-svn-id: https://mpjsp.svn.codeplex.com/svn@68529 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ emailserver/src/main/java/com/agiledon/agile/refactoring/email/FileRoster.java
@@ -0,0 +1,13 @@
+package com.agiledon.agile.refactoring.email;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: twer
+ * Date: 2/19/12
+ * Time: 1:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FileRoster implements Roster {
+    public FileRoster(String fileName) {
+    }
+}
